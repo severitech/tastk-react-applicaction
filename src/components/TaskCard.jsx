7791c05ae943a8e4adc5cffdf9e3f72eecc7d90f
severitech <padilladouglas6@gmail.com>
2024-07-20 01:56:22 -0400

    corrigiendo errores

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -7,13 +7,8 @@ function TaskCard({task}) {
   const {deleteTask}=useContext(TaskContext)
   return (
     <div className='bg-gray-800 text-white p-4 rounded-md'>
-<<<<<<< HEAD
       <h1 className='text-3xl font-bold capitalize'>{task.title}</h1>
       <p className='text-gray-500 text-xl text-x1'>{task.description}</p>
-=======
-      <h1 className='text-xl font-bold capitalize'>{task.title}</h1>
-      <p className='text-gray-500 text-sm text-x1'>{task.description}</p>
->>>>>>> 1f519a22da12b6b8f034f219b3461d0710bd5f73
       <button className='bg-red-500 rounded-md px-2 py-1 m mt-4 hover:bg-red-700' onClick={() => deleteTask(task.id)}>Eliminar Tarea</button>
     </div>
   );
--- src/components/TaskList.jsx
+++ src/components/TaskList.jsx
@@ -8,17 +8,10 @@ function TaskList() {
 const {tasks} = useContext(TaskContext)
 
   if (tasks.length === 0) {
-<<<<<<< HEAD
     return <h1 className='text-white text-4xl font-bold text-center'>No hay tareas aún</h1>;
   }
   return (
     <div className='grid grid-cols-3 gap-2 '>
-=======
-    return <h1 className='text-white text-4xl font-bold text-center'>No hay tareas aun</h1>;
-  }
-  return (
-    <div className='grid grid-cols-4 gap-2 '>
->>>>>>> 1f519a22da12b6b8f034f219b3461d0710bd5f73
       {tasks.map((task) => (
         <TaskCard key={task.id} task= {task}/>
       ))}
